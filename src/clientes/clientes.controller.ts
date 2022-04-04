0868700c5003ab410a9d5c3bd1dddcc41236a001
Maurício Citadini Biléssimo <mauricioc.bilessimo@gmail.com>
2022-04-04 14:43:10 -0300

    Fazendo cadastro de clientes.

--- src/clientes/clientes.controller.ts
+++ src/clientes/clientes.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Cliente } from './cliente.entity';
+import { ClientesService } from './clientes.service';
 
 @Controller('clientes')
-export class ClientesController {}
+export class ClientesController {
+    constructor(private readonly clientesService: ClientesService) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    async getClientes() {
+        return this.clientesService.findAll();
+    }
+
+    @Post()
+    async create(@Body() cliente: Cliente) {
+        const response = await this.clientesService.create(cliente);
+        
+        if(HttpStatus.OK) {
+            return {
+                status: 'success',
+                message: 'Cliente cadastrado com sucesso.',
+                data: response
+            };
+        }
+        else {
+            return {
+                status: 'error',
+                message: 'Problemas ao cadastrar cliente.',
+                data: response
+            };
+        }
+    }
+}
--- src/clientes/clientes.service.ts
+++ src/clientes/clientes.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Cliente } from './cliente.entity';
 
 @Injectable()
-export class ClientesService {}
+export class ClientesService {
+    constructor(
+        @InjectRepository(Cliente) private clientesRepository: Repository<Cliente>
+    ) {}
+
+    async findAll(): Promise<Cliente[]> {
+        return await this.clientesRepository.find();
+    }
+
+    async create(cliente: Cliente): Promise<Cliente> {    
+        const response = await this.clientesRepository.save(cliente);
+        return response;
+    }
+}
